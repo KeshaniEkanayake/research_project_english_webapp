74d3ed8100df76436ef58436b69e217dd1b4471f
KeshaniEkanayake <dimagikeshani5@gmail.com>
2023-08-07 15:42:20 +0530

    created buttons for 3 prof levels with colors

--- src/Views/SelectProfLevel.vue
+++ src/Views/SelectProfLevel.vue
@@ -2,7 +2,15 @@
     <div class="selectProfLevel">
       <h1>Select proficiency level</h1>
       <p>Welcome to our website! We are a company that specializes in creating awesome products.</p>
+
+      <!-- buttons to select different proficiency levels -->
       <ButtonBasicProfLevel/>
+      <br>
+      <ButtonIntermediateProfLevel/>
+      <br>
+      <ButtonAdvancedProfLevel/>
+
+
     </div>
   </template>
   
@@ -10,9 +18,15 @@
 //   related to component
   import { defineComponent } from "vue"; 
   import ButtonBasicProfLevel from "../components/ButtonBasicProfLevel.vue"
+  import ButtonIntermediateProfLevel from "../components/ButtonIntermediateProfLevel.vue"
+  import ButtonAdvancedProfLevel from "../components/ButtonAdvancedProfLevel.vue"
 
   export default defineComponent({
-    components:{ButtonBasicProfLevel},
+    components:{
+        ButtonBasicProfLevel,
+        ButtonIntermediateProfLevel,
+        ButtonAdvancedProfLevel
+    },
   });
   </script>
   
---
+++ src/components/ButtonIntermediateProfLevel.vue
@@ -0,0 +1,12 @@
+<!-- button for Intermediate proficiency level -->
+<template>
+    <n-space>
+      <n-button type="primary" size="large" class="prof-buttons">
+        Intermediate Level
+      </n-button> 
+    </n-space>
+</template>
+
+<style>
+@import '../assets/CSS/cssFile.css';
+</style>
