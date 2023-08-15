d6cdc804420624e32c4fa5d3dd79e8199b7f38e0
KeshaniEkanayake <dimagikeshani5@gmail.com>
2023-08-15 18:19:28 +0530

    create a white box using <div> to be located in the middle of the page

--- src/Views/BasicGrammarLesson1_1.vue
+++ src/Views/BasicGrammarLesson1_1.vue
@@ -1,25 +1,42 @@
 <template>
-    <div class="page-style">  
-         <CardMiddle/>
-    </div>
+    <div class="page-style">
 
+                <div class="box-property">
+                    ajfaef <br>
+                    ajifjweifji
+                </div>
+           
+    </div>
 </template>
 
-<script>
-import CardMiddle from '../components/CardMiddle.vue'
 
+
+<script>
 export default {
-    components:{
-        CardMiddle
-    }
+    
 }
 </script>
+
+
 
 <style scoped> 
     .page-style{
         display: flex;
         justify-content: center;
-        align-items: center; /* For vertical alignment */
-        height: 100vh; /*Adjust the height as needed. This locates the box properly */
+        /* For vertical alignment */
+        align-items: center; 
+        /* Adjust the height as needed. This locates the box properly */
+        height: 100vh; 
+     } 
+
+    .box-property {
+        /* white box in the middle of the page */
+        height: 50%;
+        width: 70%;
+        border-radius: 1.25rem;
+        background: #FFF;
+        box-shadow: 0px 4px 4px 5px rgba(0, 0, 0, 0.25);
     }
+
+
 </style>
