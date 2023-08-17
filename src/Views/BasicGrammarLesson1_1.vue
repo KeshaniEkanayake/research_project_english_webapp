fbf00c151cf1b1882ac25b3d7f67be17c54adb71
KeshaniEkanayake <dimagikeshani5@gmail.com>
2023-08-17 09:45:36 +0530

    added radio buttons to BasicGrammarLesson1_1.vue page

--- src/Views/BasicGrammarLesson1_1.vue
+++ src/Views/BasicGrammarLesson1_1.vue
@@ -1,20 +1,63 @@
+<!-- /Basic/Grammar/AdvPrepo   use this path to get the page displayed in th browser -->
 <template>
     <div class="page-style">
+        <div class="box-property">
+            <div class="inside-box">
+                <h4>Choose the suitable preposition</h4> <br>
+                <p>I am really proud__________ of you!</p>
 
-                <div class="box-property">
-                    ajfaef <br>
-                    ajifjweifji
-                </div>
-           
+                <n-space>
+                    <n-radio
+                        :checked="checkedValue === 'of'"
+                        value="of"
+                        name="answers"
+                        @change="handleChange"
+                    >
+                    of
+                    </n-radio>
+
+                    <n-radio
+                        :checked="checkedValue === 'to'"
+                        value="to"
+                        name="answers"
+                        @change="handleChange"
+                    >
+                        to
+                    </n-radio>
+
+                    <n-radio
+                        :checked="checkedValue === 'with'"
+                        value="with"
+                        name="answers"
+                        @change="handleChange"
+                    >
+                        with
+                    </n-radio>
+                </n-space>
+            </div>
+        </div>
     </div>
 </template>
 
 
 
 <script>
-export default {
+import { defineComponent, ref } from "vue";
+
+// related to the radio buttons in the page
+export default defineComponent({
+  setup() {
+    const checkedValueRef = ref(null);
     
-}
+    return {
+      disabled: ref(true),
+      checkedValue: checkedValueRef,
+      handleChange(e) {
+        checkedValueRef.value = e.target.value;
+      }
+    };
+  }
+});
 </script>
 
 
@@ -29,8 +72,8 @@ export default {
         height: 100vh; 
      } 
 
+     /* white box in the middle of the page */
     .box-property {
-        /* white box in the middle of the page */
         height: 50%;
         width: 70%;
         border-radius: 1.25rem;
@@ -38,5 +81,11 @@ export default {
         box-shadow: 0px 4px 4px 5px rgba(0, 0, 0, 0.25);
     }
 
+    .inside-box{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 
+   
 </style>
