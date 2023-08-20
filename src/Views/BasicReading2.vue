e5a0aefa225f9c2519e3c6249305f258e4cc0660
KeshaniEkanayake <dimagikeshani5@gmail.com>
2023-08-20 09:37:43 +0530

    completed left card of the BasicReading2.vue page

--- src/Views/BasicReading2.vue
+++ src/Views/BasicReading2.vue
@@ -1,6 +1,10 @@
 <template>
   <h2></h2>
-  <CardReadingText/>
+  <CardReadingText 
+    :cardTitle="cardTitle"
+    :imageSrc="readingTextImage"
+    :readingText="readingText"
+  />
 </template>
 
 
@@ -8,11 +12,31 @@
 
 <script>
 import CardReadingText from "../components/CardReadingText.vue"
+import readingTextImage from "../assets/images/images-in-questions/reading1.jpg";
+
 
 export default {
     components:{
         CardReadingText,
-    }
+    },
+
+    data(){
+        return{
+            cardTitle: "Time to Relax", 
+
+            // needs to display image in the card
+            readingTextImage: readingTextImage,
+            
+            // dynamically adding a related reading text to the card
+            readingText:`Come and join our lunchtime yoga class with experienced yoga teacher Divya Bridge!
+            When? Every Tuesday at 1.30 p.m.
+            Where? Meeting Room 7
+            How much? £10 for four 30-minute classes.
+            What to bring? Comfortable clothes. Divya will provide the yoga mats.
+            How to join? Write to Sam at Sam.Holden@example.com
+            We can only take a maximum of 20 in the room, so book now!`,            
+        };
+    },
 }
 </script>
 
--- src/components/CardReadingText.vue
+++ src/components/CardReadingText.vue
@@ -1,27 +1,63 @@
 <!-- this card component contains an image and a text related to a reaading comprehension question 1 -->
 <template>
-    <n-card title="Time to Relax">
+    <n-card>
       <template #cover>
-        <img src="../assets/images/images-in-questions/reading1.jpg" class="image-style">
+        <img :src="imageSrc" class="image-style">
         </template>
-            Come and join our lunchtime yoga class with experienced yoga teacher Divya Bridge!
+          <h2>{{cardTitle}}</h2>
+            <p class="para-style"> {{ readingText }}</p>
+            <!-- Come and join our lunchtime yoga class with experienced yoga teacher Divya Bridge!
             When? Every Tuesday at 1.30 p.m.
             Where? Meeting Room 7
             How much? £10 for four 30-minute classes.
             What to bring? Comfortable clothes. Divya will provide the yoga mats.
             How to join? Write to Sam at Sam.Holden@example.com
-            We can only take a maximum of 20 in the room, so book now!
+            We can only take a maximum of 20 in the room, so book now! -->
     </n-card>
   </template>
   
-  <style scoped>
+
+
+<script>
+  export default({
+    props: {
+
+      cardTitle:{
+        type: String,
+
+      },
+
+      readingText:{
+        type: Text,
+        required: true,
+      },
+
+      // used to add an image dynamically to the card
+      imageSrc: {
+              type: String,
+              required: true, // Make the prop required
+      },
+    }
+  })
+</script>
+
+
+
+<style scoped>
   .n-card {
-    max-width: 50%;
+    max-width: 40%;
     border-radius: 1.25rem;
     box-shadow: 0px 4px 4px 5px rgba(0, 0, 0, 0.25);
   }
 
   .image-style{
     border-radius: 1.25rem;
+  }
+
+  .para-style{
+    color: black;
+    /* used to keep linebreaks and spaces as typed inside data() */
+    white-space: pre-line;
+    text-align: center;
   }
   </style>
